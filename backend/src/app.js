1573d44a18eb45ffe75c93e7b70f3b4336dad525
Ananya Singh <ananyasingh10518@gmail.com>
2026-02-07 02:28:00 +0530

    Updated CORS for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,17 @@ const generateSpecRoute = require("./routes/generateSpec");
 const mockRouter = require("./routes/mockRoute");
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+    origin: [
+      "https://api-gen-nu.vercel.app", // Vercel frontend
+      "http://localhost:5173"          // local dev (Vite)
+    ],
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+
 app.use(express.json());
 
 app.use("/generate-spec", generateSpecRoute);
